e4da411df01631931658029d2045d50202650331
Kei Mileze <161208193+KeiVo1209@users.noreply.github.com>
2025-10-07 22:11:34 +0700

    Update Kt so ngto.cpp

--- Kt so ngto.cpp
+++ Kt so ngto.cpp
@@ -3,16 +3,24 @@
 using namespace std;
 
 int main() {
-    int a, cana, b;
-    cout << "Nhap so can kiem tra:"<<endl;
+    int a, cana, b = 1;
+    cout << "Nhap so can kiem tra:" << endl;
     cin >> a;
-    if (a < 2) { cout << a << " khong phai so nguyen to"; return 0; }
+    if (a < 2) {
+        cout << a << " khong phai so nguyen to";
+        return 0;
+    }
     cana = sqrt(a);
     for (int i = 2; i <= cana; i++) {
-        if (a % i == 0) { b = 0; }
-        else b = 1;
+        if (a % i == 0) {
+            b = 0;
+            break; // thoát vòng lặp nếu tìm thấy ước số
+        }
     }
-    if (b == 1) { cout << a << " la so ngto"; }
-    else cout << a << " khong phai so nguyen to";
+    if (b == 1) {
+        cout << a << " la so nguyen to";
+    } else {
+        cout << a << " khong phai so nguyen to";
+    }
     return 0;
 }
