a5758f679fba57c1c1813840c456e7da13997f27
Kei Mileze <161208193+KeiVo1209@users.noreply.github.com>
2025-11-12 17:46:47 +0700

    Improve input handling and output for three-digit numbers
    Refactor input validation and output formatting for three-digit number.

--- baicuaMan.cpp
+++ baicuaMan.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	int n;
-	do {
-		cout << "Nhap n: ";
-		cin >> n;
-		if (n >= 100 || n <= 999) {
-			cout << "n hop le ";
-		}
-		else cout << " n ko hop le ";
-	} while (n < 100 || n>999);
-	int dv, t, c;
-	dv = n % 10;
-	t = n / 100;
-	c = (n / 10) % 10;
-	cout << "Hang don vi: " << dv;
-	cout << "Hang chuc " << c;
-	cout << "hang tram:"<< t;
-	cout <<"\n"<<dv * 100 + c * 10 + t;
+    int n;
+    do {
+        cout << "Nhap n (so co 3 chu so): ";
+        cin >> n;
+        if (n >= 100 && n <= 999) {
+            cout << "n hop le\n";}
+        else {
+            cout << "n khong hop le, vui long nhap lai!\n";}
+    } while (n < 100 || n > 999);
+    int dv = n % 10, c = (n / 10) % 10,t= n / 100;
+    cout << "Hang don vi: " << dv << "\n";
+    cout << "Hang chuc: " << c << "\n";
+    cout << "Hang tram: " << t << "\n";
+    cout << "So dao nguoc: " << dv * 100 + c * 10 + t << "\n";
+    return 0;
 }
