a9fc52574eb7b07313fb2229c988518ceea7c473
Kei Mileze <161208193+KeiVo1209@users.noreply.github.com>
2025-11-21 22:58:09 +0700

    Update ktsnt.cpp

--- ktsnt.cpp
+++ ktsnt.cpp
@@ -1,22 +1,28 @@
-// ontap.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include <cmath>
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main()
-{
-	int n,r=1;
-	cin >> n;
-	if (n < 2) {
-		cout << "Ko phai nto"; return 0;
-	}
-	for (int i = 2; i < sqrt(n); i++) {
-		if (n % i == 0) {
-			r = 0; break;
-		}
-	}
-	if (r == 0) cout << "Ko phai nto";
-	else cout << "La so nto";
 
+int main() {
+    int n;
+    cin >> n;
 
+    if (n < 2) {
+        cout << "Ko phai nto";
+        return 0;
+    }
+
+    bool kt = true;
+    int cann = sqrt(n);
+
+    for (int i = 2; i <= cann; i++) {
+        if (n % i == 0) {
+            kt = false;
+            break;
+        }
+    }
+
+    if (kt) cout <<n<< " La so nto";
+    else cout <<n<< " Ko phai nto";
+
+    return 0;
 }
